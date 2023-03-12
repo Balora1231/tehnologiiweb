26964fd3be9abd32f1face0cbbe984ea5422458f
Pavel <92945495+Balora1231@users.noreply.github.com>
2023-03-11 17:18:06 -0800

    creat lab 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
+import RecipeReviewCard from "./componets/card";
+
+import ButtonBase from "./componets/button";
+import CountrySelect from "./componets/numberinput";
+import Asynchronous from "./componets/cardlist2";
+import DisabledOptions from "./componets/cardlist3";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class='items'>
+      <div>
+   <h1 class='items'>Menu</h1>
+      </div>
+      <div class="menu">
+          <div class="border">
+<RecipeReviewCard />
+      </div>
+          <div class="border">
+<RecipeReviewCard/>
+      </div>
+          <div className="border">
+              <RecipeReviewCard/>
+          </div>
+          <div className="border">
+              <RecipeReviewCard/>
+          </div>
+          <div className="border">
+              <RecipeReviewCard/>
+          </div>
+      </div>
+          <div class="numberalign">
+              <div class='inputaligns'>
+                  <CountrySelect />
+              </div>
+              <div class='inputaligns'>
+                  <Asynchronous/>
+              </div>
+              <div class='inputaligns'>
+                  <DisabledOptions/>
+              </div>
+              <div class="inputaligns">
+                  <ButtonBase/>
+              </div>
+      </div>
+          </div>
+
+
+
   );
 }
 
